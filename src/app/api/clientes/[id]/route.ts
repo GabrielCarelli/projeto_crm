d6052be35c150ae16dde214359a0946d95a9519b
Gabriel Ferreira Carelli <100717337+GabrielCarelli@users.noreply.github.com>
2025-04-05 15:32:14 -0300

    correction to deploy

--- src/app/api/clientes/[id]/route.ts
+++ src/app/api/clientes/[id]/route.ts
@@ -1,37 +1,37 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-export async function GET(
-    _req: Request,
-    {params}: {params:{id: string}}
-){
+export async function GET(request: NextRequest) {
+    const id = request.nextUrl.pathname.split("/").pop(); // extrai o ID da URL
+
     const cliente = await prisma.cliente.findUnique({
-        where: {id: Number(params.id)},
+        where: { id: Number(id) },
     });
 
-    if(!cliente) return NextResponse.json({error: 'Cliente não encontrado'}, {status: 404});
+    if (!cliente) {
+        return NextResponse.json({ error: 'Cliente não encontrado' }, { status: 404 });
+    }
+
     return NextResponse.json(cliente);
 }
 
-export async function PUT(
-  req: Request, 
-  {params}: {params: {id: string}}
-) {
+export async function PUT(request: NextRequest) {
+    const id = request.nextUrl.pathname.split("/").pop();
+    const data = await request.json();
 
-    const data = await req.json();
     const clienteAtualizado = await prisma.cliente.update({
-    where:{id: Number(params.id)},
-    data,
-   })
+        where: { id: Number(id) },
+        data,
+    });
+
     return NextResponse.json(clienteAtualizado);
 }
 
-export async function DELETE(
-  _req: Request,
-  {params}: {params: {id: string}}
-) {
+export async function DELETE(request: NextRequest) {
+    const id = request.nextUrl.pathname.split("/").pop();
+
     await prisma.cliente.delete({
-      where: { id: Number(params.id) },
+        where: { id: Number(id) },
     });
 
     return NextResponse.json({ message: 'Cliente deletado com sucesso' });
